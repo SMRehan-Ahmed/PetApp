7702eac72942de3196bb4cf26993d1ef825b9e60
EeshaneeAJ <eeshaneeaj24@gmail.com>
2024-07-15 19:49:33 +0530

    appointment1

--- pet_care/main/forms.py
+++ pet_care/main/forms.py
@@ -36,6 +36,35 @@ class MedicineForm(forms.ModelForm):
         fields = ['name_of_medicine', 'purpose_of_medicine', 'price', 'quantity']
 
 class AppointmentForm(forms.ModelForm):
+    TIME_CHOICES = [
+        ('09:00', '09:00 AM'),
+        ('10:00', '10:00 AM'),
+        ('11:00', '11:00 AM'),
+        ('12:00', '12:00 PM'),
+        ('13:00', '01:00 PM'),
+        ('14:00', '02:00 PM'),
+        ('15:00', '03:00 PM'),
+        ('16:00', '04:00 PM'),
+        ('17:00', '05:00 PM'),
+    ]
+
+    time = forms.ChoiceField(choices=TIME_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Appointment
-        fields = ['doctor_name', 'specialization']
+        fields = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        appointments = Appointment.objects.all()  # Fetch all appointments
+        for appointment in appointments:
+            doctor_name = appointment.doctor_name
+            specialization = appointment.specialization
+            label = f"{doctor_name} - {specialization}"
+            self.fields[f"appointment_{appointment.id}"] = forms.BooleanField(label=label, required=False)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if commit:
+            instance.save()
+        return instance
