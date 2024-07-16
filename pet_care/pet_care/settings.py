1105d46dae1dcddbbed8950c3ff321c7e2b5d168
Rehan Ahmed <rehan29.ahmed11@gmail.com>
2024-07-16 22:39:54 +0530

    Medicines Work

--- pet_care/pet_care/settings.py
+++ pet_care/pet_care/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -131,3 +131,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SESSION_COOKIE_AGE = 1209600  # 2 weeks in seconds
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# settings.py
+STATIC_URL = '/static/'
+
+# Additional settings for static files (if necessary)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
